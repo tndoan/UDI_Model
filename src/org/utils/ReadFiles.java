af06fd5e58da951778cecc75e82e0146e81b89bb
tndoan <doan.thanh.nam.1988@gmail.com>
2015-09-09 12:46:40 +0800

    change format of input file

--- src/org/utils/ReadFiles.java
+++ src/org/utils/ReadFiles.java
@@ -13,7 +13,8 @@ import org.object.TweetObject;
 public class ReadFiles {
 	
 	/**
-	 * 
+	 * each line of file has format: 
+	 * <id of user><space><lat>:<lng>
 	 * @param filename
 	 * @return
 	 * @throws IOException
@@ -21,21 +22,21 @@ public class ReadFiles {
 	public static HashMap<String, PointObject> readProfile(String filename) throws IOException{
 		HashMap<String, PointObject> result = new HashMap<>();
 
-		int counter = 1;
 		File file = new File(filename);
 	    FileReader fr = new FileReader(file);
 	    BufferedReader br = new BufferedReader(fr);
 	    String line;
 	    while((line = br.readLine()) != null){
-	    	if (!line.trim().equals("?")){
-	    		// if we know the location of user
-	    		String[] comp = line.trim().split(",");
-	    		double lat = Double.parseDouble(comp[0]);
-	    		double lng = Double.parseDouble(comp[1]);
+	    	String[] comp = line.split(" ");
+	    	String id = comp[0].trim(); // id of user or venue
+	    	
+	    	if (!comp[1].equals("?")){
+	    		String[] ll = comp[1].split(",");
+	    		double lat = Double.parseDouble(ll[0]);
+	    		double lng = Double.parseDouble(ll[1]);
 	    		PointObject p = new PointObject(lat, lng);
-	    		result.put(Integer.toString(counter), p);
+	    		result.put(id, p);
 	    	}
-	        counter++;
 	    }
 	    br.close();
 	    fr.close();
@@ -44,7 +45,9 @@ public class ReadFiles {
 	}
 	
 	/**
-	 * 
+	 * each line of file has format
+	 * <id of user 1><space><id of user 2>
+	 * user 1 follows user 2
 	 * @param filename
 	 * @return hash map whose key is user id, value is list of users who he follows
 	 * @throws IOException
@@ -76,7 +79,8 @@ public class ReadFiles {
 	}
 	
 	/**
-	 * 
+	 * each line of file has format
+	 * <venue id><space><user id>:<number of tweet/checkin>
 	 * @param filename
 	 * @return
 	 * @throws IOException 
@@ -84,27 +88,26 @@ public class ReadFiles {
 	public static HashMap<String, ArrayList<TweetObject>> readTweet(String filename) throws IOException{
 		HashMap<String, ArrayList<TweetObject>> result = new HashMap<>();
 		
-		int counter = 1;
 		File file = new File(filename);
 	    FileReader fr = new FileReader(file);
 	    BufferedReader br = new BufferedReader(fr);
 	    String line;
 	    while((line = br.readLine()) != null){
-	    	String[] comp = line.trim().split(",");
+	    	String[] comp = line.split(" ");
+	    	String id = comp[0].trim();
 	    	
 	    	ArrayList<TweetObject> l = new ArrayList<>();
-	    	for (int i = 0; i < comp.length; i++){
-	    		int numTweet = Integer.parseInt(comp[i]);
+	    	for (int i = 1; i < comp.length; i++){
+	    		String[] pair = comp[i].split(":");
+	    		int numTweet = Integer.parseInt(pair[1]);
 	    		if (numTweet != 0){
-	    			TweetObject to = new TweetObject(Integer.toString(i + 1), numTweet);
+	    			TweetObject to = new TweetObject(pair[0].trim(), numTweet);
 	    			l.add(to);
 	    		}
 	    	}
 	    	
 	    	if (l.size() != 0)
-	    		result.put(Integer.toString(counter), l);
-	    	
-	    	counter++;
+	    		result.put(id, l);
 	    }
 	    br.close();
 	    fr.close();
